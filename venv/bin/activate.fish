b23f58e84183cf32a5c3b7d59e400e6f9de2ea1c
Daniel Dominguez Ramos <daniel.dominguez.ramos@estudiante.uac.edu.mx>
2026-05-20 13:12:27 -0600

    Actualizacion productos y docker

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/soria/Optica/venv
+set -gx VIRTUAL_ENV /home/obxix/Optica/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
